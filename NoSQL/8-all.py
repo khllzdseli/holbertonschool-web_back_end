dc655d485197e81921c0f8137952a98d03637619
khllzdseli <princesskhalilzadeh@gmail.com>
2026-05-06 21:36:37 +0400

    Create 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 8-all module """
+
+
+def list_all(mongo_collection):
+    """ Lists all documents in a collection """
+    schools = mongo_collection.find()
+    return [school for school in schools] if schools else []
