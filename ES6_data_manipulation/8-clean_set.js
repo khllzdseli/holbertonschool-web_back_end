87eb294aeeef11741109c22a663854a268a25d06
khllzdseli <princesskhalilzadeh@gmail.com>
2026-05-15 18:25:53 +0400

    Create 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  const result = [];
+  set.forEach((value) => {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+  return result.join('-');
+}
